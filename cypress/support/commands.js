5f52317f18a2771105ea8dbc269612c2d116bd05
ryujoo <75553371+ryujoo@users.noreply.github.com>
2026-01-19 16:42:29 -0800

    Update commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -6,13 +6,7 @@ Cypress.Commands.add('login', (username, password) => {
 
   cy.get('input[name="username"]').type(username)
   cy.get('input[name="password"]').type(password)
-  cy.get('button[type="submit"]').click()
-  /*
-  LoginPage.visit()
-  LoginPage.typeUsername(username)
-  LoginPage.typePassword(password)
-  LoginPage.clickLogin()
-  */
+  cy.get('button[type="submit"]').click() 
 })
 
 
