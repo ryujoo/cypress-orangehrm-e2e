cf5b1c2ce7e02fa9338521d4c3c5b7b75d078542
ryujoo <jooyeon@zy-MacBook-Pro.local>
2026-01-23 00:12:09 -0800

    Stabilize Cypress CI execution

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require("cypress");
 
-module.exports = defineConfig({
+export default defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
   },
-});
+})
