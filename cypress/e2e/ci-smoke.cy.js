6a0ef0488445f048477910c729640d4c0e45acf7
ryujoo <jooyeon@zy-MacBook-Pro.local>
2026-01-26 19:57:02 -0800

    Add logout validation to CI smoke test

--- cypress/e2e/ci-smoke.cy.js
+++ cypress/e2e/ci-smoke.cy.js
@@ -1,12 +1,21 @@
 describe('CI Smoke Test', () => {
-  it('Logs in successfully', () => {
+  it('Logs in and logs out successfully', () => {
+    // Login
     cy.login('Admin', 'admin123')
 
-    // Dashboard loade
+    // Assert Dashboard loaded
     cy.url({ timeout: 15000 }).should('include', '/dashboard')
     cy.contains('Dashboard', { timeout: 15000 }).should('be.visible')
-
-    // Sidebar exists (critical app shell check)
     cy.get('.oxd-sidepanel', { timeout: 15000 }).should('be.visible')
+
+    // Open user dropdown
+    cy.get('.oxd-userdropdown-name', { timeout: 15000 }).click()
+
+    // Click Logout
+    cy.contains('Logout', { timeout: 15000}).click()
+
+    // Assert redirected to login page
+    cy.url({ timeout: 150000}).should('include', '/auth/login')
+    cy.contains('Login', { timeout: 15000 }).should('be.visible')
   })
 })
